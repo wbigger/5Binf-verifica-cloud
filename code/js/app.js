7093c6f9cc1e17a74d6f456be5eb9727b453c849
Your Name <you@example.com>
2019-04-16 11:03:56 +0200

    consegna compito

--- code/js/app.js
+++ code/js/app.js
@@ -5,8 +5,56 @@ var app = {
 		.done(app.showCards)
 		.fail(app.onError);
 	},
-	showCards(dataJson) {
-		console.log(dataJson);
+	showCards(dataJson){
+	console.log(dataJson);
+		dataJson.cardList.forEach(element => {
+			let card = `div class="card-body">
+		<div class="table-responsive">
+		  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+			<thead>
+			  <tr>
+				<th>Nome</th>
+				<th>Cognome</th>
+				<th>Materia</th>
+			  </tr>
+			</thead>
+			<tfoot>
+			  <tr>
+				<th>Nome</th>
+				<th>Cognome</th>
+				<th>Materia</th>
+			  </tr>
+			</tfoot>
+			<tbody>
+			  <tr>
+				<td>${element.nome}</td>
+				<td>${element.cognome}</td>
+				<td>${element.materia}</td>
+			  </tr>
+			  <tr>
+				<td>${element.nome}</td>
+				<td>${element.cognome}</td>
+				<td>${element.materia}</td>
+			  </tr>
+			  <tr>
+				<td>${element.nome}</td>
+				<td>${element.cognome}</td>
+				<td>${element.materia}</td>
+			  </tr>
+			</tbody>
+		  </table>
+		</div>
+	  </div>
+	  <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+	</div>`
+	$("card-container").append(card)
+			
+		});
+		
+
+		
+	
+
 		
 	},
 	onError: function(e) {
