013ab20d5904dd65ed216dfdce89a19aad487085
daniele leo <danieleleo16@gmail.com>
2019-04-16 10:59:03 +0200

    create dinamic page

--- code/js/app.js
+++ code/js/app.js
@@ -7,6 +7,14 @@ var app = {
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
+		dataJson.tableList.forEach(element => {
+			let table = `<tr>
+			<td>${element.nome}</td>
+			<td>${element.cognome}</td>
+			<td>${element.materia}</td>
+			</tr>`;
+		$("#table-container").append(table);
+		});
 		
 	},
 	onError: function(e) {
