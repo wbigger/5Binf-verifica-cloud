42857666b4185a720b9883b4815a20d09285b8f5
INF3-4 <fabri00>
2019-04-16 11:05:42 +0200

    update js and json

--- code/js/app.js
+++ code/js/app.js
@@ -1,18 +1,39 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.showCards)
-		.fail(app.onError);
+			.done(app.showCards,app.onSuccess)
+			.fail(app.onError);
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+
 	},
-	onError: function(e) {
+	onSuccess: function (jsonData) {
+		
+		let list = jsonData.commList;
+		list.forEach(element => {
+			let html = `<tr>
+			<td>${element.name}</td>
+			<td>${element.surname}</td>
+			<td>${element.subject}</td>
+		  </tr>`;
+			$("#dataTable tbody").append(html);
+		});
+
+		let list2 = jsonData.lastUpdate;
+		list2.forEach(element => {
+			let html2 = `<div class="card-footer small text-muted">Updated ${element.day} at ${element.time}</div>`;
+			$("#card").after(html2);
+		});
+
+	},
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
 }
 
 $(document).ready(app.init);
+
+
