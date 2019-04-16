b5c53938d963c867fc2421dc69d5644cc628acc9
brecciaaa <peterbreccia@gmail.com>
2019-04-16 11:40:38 +0200

    screenshot

--- code/js/app.js
+++ code/js/app.js
@@ -5,10 +5,19 @@ var app = {
 		.done(app.showCards)
 		.fail(app.onError);
 	},
-	showCards(dataJson) {
-		console.log(dataJson);
-		
+	showCards(datajson) {
+		console.log(datajson);
+	
+		datajson.commissionList.forEach(element => {
+			let card=` <tr>
+			<td>${element.name}</td>
+			<td>${element.surname}</td>
+			<td>${element.materia}</td>
+		  </tr>`;
+			$("#esame").append(card);
+		});
 	},
+
 	onError: function(e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
