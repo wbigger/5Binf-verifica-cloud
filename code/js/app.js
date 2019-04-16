a76d8b3a2e6208e85059415d60898362b2bb95aa
micia01 <miriamyounes.my@gmail.com>
2019-04-16 11:05:27 +0200

    done js and json

--- code/js/app.js
+++ code/js/app.js
@@ -2,12 +2,29 @@ var app = {
 	init: function() {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.showCards)
+		.done(app.showCards,app.onSuccess)
 		.fail(app.onError);
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
+
 		
+	},
+	onSuccess:function(jsonData){
+		let list = jsonData.commissionList;
+		   list.forEach(element => {
+			   let html = `<tr>
+			   <td>${element.name}</td>
+			   <td>${element.surname}</td>
+			   <td>${element.subject}</td>
+			 </tr>`;
+		   $("#dataTable tbody").append(html);
+		   });
+		let listup = jsonData.update;
+		listup.forEach(element => {
+			let upd = `<div class="card-footer small text-muted">Updated ${element.date} at ${element.time}</div>`;
+		$("#card").after(upd);
+		});
 	},
 	onError: function(e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
