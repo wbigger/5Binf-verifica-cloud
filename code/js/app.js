a33da71ddccf0b7a4b8c012f29e5314a1f811408
Your Name <you@example.com>
2019-04-16 11:04:11 +0200

    Create dinamic page

--- code/js/app.js
+++ code/js/app.js
@@ -1,16 +1,26 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.showCards)
-		.fail(app.onError);
+			.done(app.showCards)
+			.fail(app.onError);
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+		dataJson.tableList.forEach(element => {
+			let table = `<tr>
+			<td>${element.name}</td>
+			<td>${element.cognome}</td>
+			<td>${element.materia}</td>
+		  </tr>`
+			$("#table-container").append(table);
+
+		});
+
 	},
-	onError: function(e) {
-		console.log(`Ajax error: ${JSON.stringify(e)}`);
+	onError: function (e) {
+		console.log(`Ajax error: ${JSON.stringify(e)
+			}`);
 	}
 
 }
