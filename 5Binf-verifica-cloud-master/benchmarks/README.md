3ccbdb3ce8e72d9a2d05be81428de5bc25fbcdb0
wbigger <wbigger@gmail.com>
2019-04-29 15:52:20 +0200

    Correzione docente

--- 5Binf-verifica-cloud-master/benchmarks/README.md
+++
@@ -1,1 +0,0 @@
-Put your benchmarks in this folder.
--- code/js/app.js
+++ code/js/app.js
@@ -6,13 +6,17 @@ var app = {
 		.fail(app.onError);
 	},
 	showCards(dataJson) {
-		console.log(dataJson);
+		console.log(dataJson)
+		dataJson.list.forEach(element => {let materia = `<tr>
+		<td>${element.nome}</td>
+		<td>${element.cognome}</td>
+		<td>${element.materia}</td>
+	  </tr>`});
+	  $("#materia").append(materia);
 		
 	},
 	onError: function(e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
-	}
-
+},
 }
-
 $(document).ready(app.init);
